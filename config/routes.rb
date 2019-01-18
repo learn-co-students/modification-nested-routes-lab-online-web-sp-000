afcfddd6810e53879421fa27b884a6ade7be907b
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-18 17:07:50 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:index, :show]
+    resources :songs, only: [:show, :index, :new, :edit]
   end
+  
   resources :songs
 end
