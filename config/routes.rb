0311dd313b1b2cc4869949c0ceba80dafb74d981
Douglas Uretsky <douglasuretsky@gmail.com>
2019-01-31 08:59:46 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show, :edit, :new]
   end
-  resources :songs
+  resources :songs do
+    resources :artists
+  end
 end
