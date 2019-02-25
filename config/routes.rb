35ed9967c89bc7430f53f0d53be342182627ee27
maxwellbenton <maxwellbenton@gmail.com>
2019-02-25 17:08:51 -0500

    adds routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists do
+    resources :songs, only: [:index, :show, :new, :edit]
+  end
+  resources :songs
 end
