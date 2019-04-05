e80c89f7affe10381e76787ea2f51c77cef2d8db
Jcg408 <jcg321@comcas.net>
2019-04-05 11:03:28 -0400

    Done.

--- db/migrate/20160126235547_add_artist_to_song.rb
+++ db/migrate/20160126235547_add_artist_to_song.rb
@@ -1,4 +1,4 @@
-class AddArtistToSong < ActiveRecord::Migration
+class AddArtistToSong < ActiveRecord::Migration[5.2]
   def change
     add_reference :songs, :artist, index: true, foreign_key: true
   end
