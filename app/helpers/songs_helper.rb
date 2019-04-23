6229562a491c4aabaa030848c830d740436c0cc5
Rafael Batista <RafaelB24@yahoo.com>
2019-04-22 20:01:11 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
+ def display_artist(song)
+   song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+ end
 
+  def artist_select(song, artist)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[author_id]", artist.id
+    end
+  end
 end
