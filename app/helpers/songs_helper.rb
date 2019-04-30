27e7b452b39b1fcfeb09a427a3227a1f4590b48e
learn <36858744+decentralvision@users.noreply.github.com>
2019-04-29 18:11:14 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+    def artist_id_field(song)
+        if song.artist.nil?
+          artist_select(song, song.artist)
+        else
+          hidden_field_tag "song[artist_id]", song.artist_id
+        end
+      end
 end
