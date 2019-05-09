a24ec447eaa825efe27bc36883de091147cb527b
Tyler Hunt <tyhunt04@gmail.com>
2019-05-09 17:16:55 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,12 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artist)
+    if artist.nil?
+      collection_select(:song, :artist_id, Artist.all, :id, :name)
+    else
+      link_to(artist.name, artist_path(artist))
+    end
+  end
 end
