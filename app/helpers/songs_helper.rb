648e953a91cfdfcf7225346ac14c2159631d13f3
Jared Grove <jaredkgrove@gmail.com>
2019-08-09 12:54:25 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+    def artist_select(artist, song)
+        if artist.nil?
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        else
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        end
+      end
 end
