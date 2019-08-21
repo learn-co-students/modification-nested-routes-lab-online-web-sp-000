4dedd2b6efdb363238ee09b22c25ab95417e975d
Alan Krajina <alankrajina88@gmail.com>
2019-08-21 13:50:24 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,9 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+
+  def artist-select(artist,song)
+
+  end
 end
