d43200a98f1e28cb3ee66851cd43ac69b433688e
donna <donnacamos88@gmail.com>
2019-08-30 14:28:45 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -39,7 +43,16 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    artist_exists = Artist.exists? params[:artist_id]
+    if params[:artist_id] and not artist_exists
+      redirect_to artists_path, alert: "Artist not found"
+    elsif params[:artist_id] and artist_exists
+      if not (@song = Song.joins(:artist).where(artist_id: params[:artist_id]).where(id: params[:id]).first)
+        redirect_to artist_songs_path(params[:artist_id]), alert: "Song not found"
+      end
+    else
+      redirect_to(songs_path, alert: "Song not found") if not (@song = Song.find_by(id: params[:id]))
+    end
   end
 
   def update
@@ -64,7 +77,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,12 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, making_tests_pass_with_variables_i_dont_need = "waste of my time")
+    if song.artist.nil?
+        select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+       sanitize "<p>#{song.artist.name}</p>"
+    end
+end
 end
