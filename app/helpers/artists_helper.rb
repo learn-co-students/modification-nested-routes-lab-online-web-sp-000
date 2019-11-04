fda70791c12848d8b348524aa6977b24104999fa
Mohawk Greene <kellyecodes@gmail.com>
2019-11-04 14:32:02 -0500

    Fixes #11

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,11 +3,19 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select(artist, song)
-    if artist
-      artist.name
+  # Display dropdown of artists, with name if editing through nested
+  def artist_select(song, path)
+    if song.artist && path == "nested"
+      hidden_field_tag "song[artist_id]", song.artist_id
     else
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
+  end
+
+  # Display name if editing through nested
+  def display_name(song, path)
+    if song.artist && path == "nested"
+      song.artist.name
     end
   end
 end
