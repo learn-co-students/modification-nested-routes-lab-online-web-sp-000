f2187fe1e672dff69f3a74b45e4734ff53052bb3
Paul Rosero <paul_rosero@yahoo.com>
2019-11-11 18:13:40 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,7 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    @song = Song.new(artist_id: params[:artist_id])
   end
 
   def create
@@ -64,7 +64,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
