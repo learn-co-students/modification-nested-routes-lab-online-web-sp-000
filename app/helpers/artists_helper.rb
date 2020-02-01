93dc6ec79f8cd5024842bc0a70da6fdd7ff4105e
juan siu <juan.siu@gmail.com>
2020-02-01 02:28:42 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,14 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  #def artist_select(song, artists)
+  #  if song.artist == nil
+  #collection_select artists
+  #  else
+  #name = song.artist.name
+  #name
+  #  end
+  #end
+
 end
