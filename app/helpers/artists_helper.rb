415f6b3e828238885523dde66645ec92287c9fb6
Keith Isham <kisham8899@gmail.com>
2020-02-17 16:28:40 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,20 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  # Display dropdown of artists, with name if editing through nested
+  def artist_select(song, path)
+    if song.artist && path == "nested"
+      hidden_field_tag "song[artist_id]", song.artist_id
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
+  end
+
+  # Display name if editing through nested
+  def display_name(song, path)
+    if song.artist && path == "nested"
+      song.artist.name
+    end
+  end
 end
