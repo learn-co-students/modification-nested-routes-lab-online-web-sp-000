7de01aac705f694bbfaaef28bf831a3616ca8e4c
dayofgray <graysonpberger@gmail.com>
2020-06-07 12:24:44 -0400

    Revert "Done."
    This reverts commit 7c6e82bd5caebe9bae67079dbc32853ee7e4d2a3.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,11 +25,7 @@ class SongsController < ApplicationController
   end
 
   def new
-    if params[:artist_id] && !Artist.exists?(id: params[:artist_id])
-      redirect_to artists_path, alert: "Artist not found"
-    else
-      @song = Song.new(artist_id: params[:artist_id])
-    end
+    @song = Song.new
   end
 
   def create
@@ -43,20 +39,7 @@ class SongsController < ApplicationController
   end
 
   def edit
-    if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
-      else
-        @song = @artist.songs.find_by(id: params[:id])
-        if @song.nil?
-          redirect_to artist_songs_path(@artist), alert: "Song not found"
-        end
-      end
-    else
-      @song = Song.find(params[:id])
-    end
-
+    @song = Song.find(params[:id])
   end
 
   def update
@@ -81,7 +64,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :artist_id)
+    params.require(:song).permit(:title, :artist_name)
   end
 end
 
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,11 +1,3 @@
 module SongsHelper
 
-  def artist_select(artist, song)
-    if artist
-      artist.name
-    else
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    end
-  end
-
 end
