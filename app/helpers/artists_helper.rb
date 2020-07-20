bb41bac3ff571efd6b2e0ef8c6f65e73d85a0282
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-19 20:27:08 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,17 @@
 module ArtistsHelper
+  def artist_select(artist, song)
+    if artist
+      artist.name
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
+  end
+
   def display_artist(song)
-    song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+    if song.artist.nil?
+      link_to "Add Artist", edit_song_path(song)
+    else
+      link_to song.artist.name, artist_path(song.artist)
+    end
   end
 end
