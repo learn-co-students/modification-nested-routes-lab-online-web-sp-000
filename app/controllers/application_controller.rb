3c782ea3bfabc99d8c273718a5fc00552f6d9f72
Snowing222 <snowing222@gmail.com>
2020-10-10 15:08:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :find_artist_song, :find_song
+
+  def find_artist_song(artist_id, song_id)
+    if @artist = Artist.find_by(id:artist_id)
+      if @song = @artist.songs.find_by(id:song_id)
+        @song
+      else
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end
+    else
+      redirect_to artists_path, alert: "Artist not found"
+    end
+  end
+
+def find_song(song_id)
+    if @song = Song.find_by(id: song_id)
+      @song
+    else
+      redirect_to songs_path, alert: "Song not found"
+    end
+end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
+    def artist_id_field(song)
+        if song.artist
+            hidden_field_tag "song[artist_id]", song.artist_id
+        else
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name), include_blank: true
+        end
+    end
 
 end
