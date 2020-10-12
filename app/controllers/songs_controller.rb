8f513a5231b4c02563ebeddeb925bd2568942930
ArslanInfinite <ali.arslan319@gmail.com>
2020-10-11 22:34:41 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,13 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    if params[:artist_id]
+      if Artist.find_by_id(params[:artist_id])
+        @song.artist_id = params[:artist_id]
+      else
+        redirect_to artists_path
+      end
+    end 
   end
 
   def create
@@ -39,7 +46,18 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    @display_name = ""
+    if params[:artist_id]
+      artist = Artist.find_by_id(params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path
+      else
+        @song = artist.songs.find_by_id(params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song does not belong to artist's songs" if @post.nil?
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +82,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
