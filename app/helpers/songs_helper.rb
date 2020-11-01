303d578c6586f03053fc62c819a5f0bf9c3639df
jake0988 <jake098877@gmail.com>
2020-10-31 20:30:25 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+    def song_artist_name_field(song)
+        if song.artist.nil?
+            select_tag "song[artist_id]", collection_check_boxes(:song,:artist_id, Artist.all, :id, :name)
+        else
+            hidden_field_tag "song[artist_id]", song.artist_id
+        end
+    end
 end
