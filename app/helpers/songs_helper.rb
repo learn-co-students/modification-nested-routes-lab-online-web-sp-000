ae7bd4dc9f7a65e26dac54c22dfc2ae2544cf2a6
daniel_feleppa <dfeleppa@gmail.com>
2021-01-21 20:36:27 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+    def artist_select(song)
+        if song.artist.nil?
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        else
+            f.label :artist_name
+            f.text_field :artist_name
+        end
+    end
 end
