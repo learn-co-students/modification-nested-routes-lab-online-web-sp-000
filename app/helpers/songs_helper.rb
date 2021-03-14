d3aa8b5ad27193a2b1327becc4b7104a785493fa
Alan Tang <giabao_tang@yahoo.com>
2021-03-14 13:48:19 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
-
+  def artist_select(song, path)
+    if song.artist && path == "nested"
+      hidden_field_tag "song[artist_id]", song.artist_id
+      song.artist.name
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
+  end
 end
+
+
