be9d3a33ac4a80bfbb3dd08206bff2f273826546
a-laz <mortaleusmarketing@gmail.com>
2021-03-21 10:16:35 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,4 +9,13 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+
+  def artist_id
+    self.try(:artist).try(:id)
+  end
+
+  def artist_id=(artist_id)
+    artist = Artist.find_by(id: artist_id)
+    self.artist = artist
+  end
 end
