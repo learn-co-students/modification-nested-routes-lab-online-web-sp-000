fefded186d02700d9252d3893a4b5449ce475b3b
dbrandt1990 <dvbrandt90@gmail.com>
2021-04-15 10:45:43 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if Artist.exists?(params[:artist_id])
+      @song = Song.new(artist_id: params[:artist_id])
+    else 
+      redirect_to artists_path
+    end
   end
 
   def create
@@ -39,7 +43,22 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    
+    @song = Song.find_by(id: params[:id])
+    
+    if params[:artist_id]
+      @path = "nested"
+      if Artist.find_by(id: params[:artist_id]) 
+        @artist = Artist.find(params[:artist_id]) 
+
+        if @song.nil?
+          redirect_to artist_songs_path(@artist)
+        end
+      else
+        redirect_to artists_path
+      end
+
+    end
   end
 
   def update
@@ -64,7 +83,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,15 @@
 module SongsHelper
 
+    def artist_select(song, path)
+        if song.artist && path == "nested"
+          label = label_tag "artist_id", "By "
+          hidden_field_tag "song[artist_id]", song.artist_id
+          label + song.artist.name
+        else
+          label = label_tag "artist_id", "Select Artist "
+          input = select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+          label + input
+        end
+      end
+
 end
