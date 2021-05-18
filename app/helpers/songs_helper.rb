d9e4b286213bc5f0c5dda31b7286ac01dcdd47c9
Ben Hayes <b.t.hayes@comcast.net>
2021-05-18 17:35:45 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+  def artist_select(song)
+    if !params[:artist_id]
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+      song.artist.name
+    end
+  end
 end
