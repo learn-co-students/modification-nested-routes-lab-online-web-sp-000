7a61b5bf4e7cc82d78070ac90a76db655ab8a5b1
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-06-04 11:34:00 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
-
+    def artist_name_field(song)
+        if song.artist.nil?
+          select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        else
+          html = "<h2>by #{song.artist.name}</h2>"
+          hidden_field_tag "song[artist_id]", song.artist_id
+          html.html_safe
+        end
+      end
 end
